d08c655cf3a2550f296e5e1aa2b702509a6ad2de
UsersByYang <2476513919@qq.com>
2025-04-07 20:04:27 +0800

    修改的

--- Geiwu Qiyun/Assets/保存C#脚本的地方/move.cs
+++ Geiwu Qiyun/Assets/保存C#脚本的地方/move.cs
@@ -1,6 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 10;
+    private Animator myAnimation;
+    private Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimation = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsWalk();
+        flip();
+    }
+    void IsWalk()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        // 判断相反键是否同时按下
+        bool oppositeX = (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D));
+        bool oppositeZ = (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S));
+
+        if (oppositeX || oppositeZ)
+        {
+            // 如果相反键同时按下，停止移动
+            moveX = 0;
+            moveZ = 0;
+        }
+
+        Vector3 playerDrection = new Vector3(moveX * speed, playerRigidbody.velocity.y, moveZ * speed);
+        playerRigidbody.velocity = playerDrection;
+
+        bool Judgement = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+        bool Judgement2 = Mathf.Abs(playerRigidbody.velocity.z) > Mathf.Epsilon && playerRigidbody.velocity.z > 0;
+        bool Judgement3 = Mathf.Abs(playerRigidbody.velocity.z) > Mathf.Epsilon && playerRigidbody.velocity.z < 0;
+
+        myAnimation.SetBool("right", Judgement);
+        myAnimation.SetBool("back", Judgement2);
+        myAnimation.SetBool("front", Judgement3);
+    }
+    //反转
+    void flip()
+    {
+        bool Judgement = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+        if (Judgement)
+        {
+            if (playerRigidbody.velocity.x > 0.1f)
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            if (playerRigidbody.velocity.x < -0.1f)
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
+
+//原代码
+/*using UnityEngine;
 
 public class move : MonoBehaviour
 {
@@ -50,4 +113,4 @@ public class move : MonoBehaviour
 
     }
    
-}
+}*/
