37b2c2fe742244da001fdd71cb2e547b4b49ed41
TANGvb <1215795758@qq.com>
2025-04-20 00:12:47 +0800

    加了脚本准备编写

---
+++ Geiwu Qiyun/Assets/保存C#脚本的地方/拼图游戏/PieceControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //鼠标点击开始，计算偏移量
+    private void dragBeginning()
+    {
+        Vector3 worldPositon;
+        if ()
+        { 
+        
+        }
+    }
+}
