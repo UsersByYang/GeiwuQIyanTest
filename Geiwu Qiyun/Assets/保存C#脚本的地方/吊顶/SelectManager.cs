0374cdbdc36ad42691afd25022108301d2dff087
TANGvb <1215795758@qq.com>
2025-04-22 00:06:39 +0800

    加了按钮选项，但是有问题

--- Geiwu Qiyun/Assets/保存C#脚本的地方/吊顶/SelectManager.cs
+++ Geiwu Qiyun/Assets/保存C#脚本的地方/吊顶/SelectManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -9,9 +9,9 @@ public class ButtonManager : MonoBehaviour
     public Animator animator;
     
     private Animator ApparatusAnimatior;
-    public DialogueManager wrongDialogue;
-    public DialogueManager correctDialogue;
-    public DialogueManager startDialogue;
+    public DialogueManager3 wrongDialogue;
+    public DialogueManager3 correctDialogue;
+    public DialogueManager3 startDialogue;
 
     private void Start()
     {
@@ -39,5 +39,46 @@ public class ButtonManager : MonoBehaviour
     }
 
    
+}*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Button correctButton;
+    public Button wrongButton;
+    public Animator animator;
+
+    private Animator ApparatusAnimatior;
+    public DialogueManager3 wrongDialogue;
+    public DialogueManager3 correctDialogue;
+    public DialogueManager3 startDialogue;
+
+    private void Start()
+    {
+        ApparatusAnimatior = GetComponent<Animator>();
+        correctButton.onClick.AddListener(CorrectButtonClicked);
+        wrongButton.onClick.AddListener(WrongButtonClicked);
+        startDialogue.StartDialogue();
+    }
+
+    private void CorrectButtonClicked()
+    {
+        Debug.Log("正确按钮被点击");
+        // 选择正确，播放动画
+        correctDialogue.StartDialogue();
+        animator.SetBool("pull", true);
+    }
+
+    private void WrongButtonClicked()
+    {
+        Debug.Log("错误按钮被点击");
+        // 选择错误，开始错误对话
+        wrongDialogue.StartDialogue();
+        // 对话结束逻辑完善后再启用
+        // correctButton.gameObject.SetActive(true);
+        // wrongButton.gameObject.SetActive(true);
+    }
 }
 
