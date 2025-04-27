f82302eaa237cc2741caef7f8d5bc8ca0fd63b2c
UsersByYang <2476513919@qq.com>
2025-04-27 20:57:23 +0800

    粮仓更改

--- Geiwu Qiyun/Assets/Inventory_bagSystem/InventoryScripts/ItemOnWorld.cs
+++ Geiwu Qiyun/Assets/Inventory_bagSystem/InventoryScripts/ItemOnWorld.cs
@@ -6,16 +6,24 @@ public class ItemOnWorld : MonoBehaviour
 {
     public Item thisItem;
     public Inventory playerInventory;
-    public bool isItemPicked=false; 
+    public bool isItemPicked=false;
+
+    private void Start()
+    {
+        MapItemManager.Instance.RegisterItem(this);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             AddNewItem();
+
+            MapItemManager.Instance.RemoveItem(this);//
+
             Destroy(gameObject);
             isItemPicked = true;
-            ItemManager.Instance.OnItemCollected(); // 通知管理器物品被收集
+            //ItemManager.Instance.OnItemCollected(); // 通知管理器物品被收集
         }
     }
 
