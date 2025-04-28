fa52f9a00af5a2e600e133016ceee0c3997fdcf0
TANGvb <1215795758@qq.com>
2025-04-28 13:47:54 +0800

    真的就差一点了

---
+++ Geiwu Qiyun/Assets/保存C#脚本的地方/Effect/AnimationEvent2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationEvent2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /* void Start()
+     {
+         StartCoroutine(DelayedStart());
+     }
+
+     IEnumerator DelayedStart()
+     {
+             yield return new WaitForSeconds(2f);
+             Event1();
+             Event2();
+     }*/
+
+    public void Event1()
+    {
+        print("ok");
+    }
+
+    public void Event2()
+    {
+        SceneManager.LoadScene("StartAgian");
+    }
+
+}
